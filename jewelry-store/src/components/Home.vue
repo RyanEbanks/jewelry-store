03b2ecb9348ca2448bb7e19a653c08ad2ab58784
RyanEbanks <ryankebanks@gmail.com>
2025-02-19 22:39:39 -0500

    Created the initial vue project

---
+++ jewelry-store/src/components/Home.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <!-- navigation bar -->
+  <div id="navigation-bar">
+    <div class="">
+      <img src="../assets/logo.svg" alt="company logo" class="">
+    </div>
+    <div>
+      <ul>
+        <li>Women</li>
+        <li>Men</li>
+        <li>Kids</li>
+      </ul>
+    </div>
+    <div>
+      <p>Cart</p>
+    </div>
+  </div>
+  <!-- Body of thhe home page -->
+  <div>
+    <div>
+      <p>Banner</p>
+    </div>
+    <div>
+      <p>Paragraphs</p>
+    </div>
+  </div>
+  <!-- Search bar -->
+  <div id="search-bar">
+    <ul>
+      <li>Home</li>
+      <li>Search</li>
+      <li>Sign in</li>
+    </ul>
+  </div>
+</template>
